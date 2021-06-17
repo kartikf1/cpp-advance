563f6fedc666513c80ca9b322251887274bf13f9
kartikf1 <kartikf1@protonmail.com>
2021-06-17 18:14:35 +0530

    added code

--- input/methods.cpp
+++ input/methods.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class MyClass
+{       // The class
+public: // Access specifier
+    void myMethod()
+    { // Method/function
+        cout << "Hello World!";
+    }
+};
+
+int main()
+{
+    MyClass myObj;    // Create an object of MyClass
+    myObj.myMethod(); // Call the method
+    return 0;
+}
+// Added comment
