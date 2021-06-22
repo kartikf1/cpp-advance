8af8574b0efb0894f194b1ecf39879b74dc73380
kartikf1 <kartikf1@protonmail.com>
2021-06-22 12:32:00 +0530

    added access specifier defination in form of comment

---
+++ input/access_specifier.cpp
@@ -0,0 +1,5 @@
+/* Access specifier can make the attributes and methods of a class 
+ Public - In this we can access attributes and methods from anywhere.
+ Private - In this we cann't access any attributes and methods from outside the class.
+ Protected - members cannot be accessed from outside the class, however, they can be accessed in inherited class.
+ */
