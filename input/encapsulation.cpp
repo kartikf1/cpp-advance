baa1b381925ec0f8803abeb94f21b195b59d61c3
kartikf1 <kartikf1@protonmail.com>
2021-06-24 12:49:37 +0530

    Written encapsulation program

---
+++ input/encapsulation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+private:
+    int salary;
+
+public:
+    // setter
+    void setSalary(int s)
+    {
+        salary = s;
+    }
+    int getSalary()
+    {
+        return salary;
+    }
+};
+main()
+{
+    employee sohan;
+    sohan.setSalary(3000);
+    cout << "The salary of sohan is : " << sohan.getSalary();
+    return 0;
+}
